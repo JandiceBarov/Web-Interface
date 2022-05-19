b25d2e728cf821053a160f68c07f64dedd433978
JandiceBarov <92557446+JandiceBarov@users.noreply.github.com>
2022-05-20 00:56:15 +0300

    Update Dz2.js

--- Dz2.js
+++ Dz2.js
@@ -24,34 +24,4 @@ else{
     }
 }
 console.log(minutes);
--------------------------------
--------------------------------
-let isRaining = false;
-let temperature = 25;
-let minutes;
-if (isRaining)
-  {
-    minutes = 0;
-  }
-else{
-  if (temperature > 35 && temperature < 0)
-    {
-      minutes = 0;
-    }
-  if (temperature == 20)
-    {
-      minutes = 20;
-    }
-  else
-    {
-      if (temperature > 20)
-        {
-          minutes = 20 - (temperature - 20);
-        }
-      if(temperature < 20)
-        {
-          minutes = 20 - (20-temperature);
-        }
-    }
-}
-alert(minutes);
+
